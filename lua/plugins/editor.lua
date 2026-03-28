1b108dc0e5d2f67d3588ea6975dd056c7101935d
José Ignacio Trava García <travag97@gmail.com>
2026-03-28 13:45:57 -0600

    chore: show hidden files in snacks

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -9,4 +9,22 @@ return {
       },
     },
   },
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            hidden = true,
+          },
+          files = {
+            hidden = true,
+          },
+          grep = {
+            hidden = true,
+          },
+        },
+      },
+    },
+  },
 }
