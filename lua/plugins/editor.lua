f64e2aa324e36cde1362d5fea09b3b57cd2a85e1
José Ignacio Trava García <travag97@gmail.com>
2026-03-30 11:48:31 -0600

    Show ignored files in Snacks explorer

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -99,6 +99,7 @@ return {
       opts.picker.sources = opts.picker.sources or {}
       opts.picker.sources.explorer = vim.tbl_deep_extend("force", opts.picker.sources.explorer or {}, {
         hidden = true,
+        ignored = true,
         win = {
           list = {
             keys = {
